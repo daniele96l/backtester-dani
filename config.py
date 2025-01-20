71001e7b4148ae755c199871ebd0b412610e31ac
danieleligato <daniele96ligato@gmail.com>
2025-01-20 20:47:59 +0100

    Loading screen

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Paths and Constants
+DEBUG = True
+if DEBUG:
+    FILE_PATH = "data/Index_list_cleaned.csv"  # Ensure this CSV is in the same directory as the script
+    ETF_BASE_PATH = "data/ETFs"
+    BASE_PATH = "data"
+else:
+    FILE_PATH = "/home/dani/backtester/backtester-dani/data/Index_list_cleaned.csv"
+    ETF_BASE_PATH = "/dani/dani/backtester/backtester-dani/data/ETFs"
+    BASE_PATH = "/home/dani/backtester/backtester-dani/data"
+
