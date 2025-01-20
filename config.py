fd7cab3b66d3f76e0d0f8b2b83ace1cfa02db310
Daniele Ligato <daniele96ligato@gmail.com>
2025-01-20 22:41:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 # config.py
 
-if DEBUG:
-    FILE_PATH = "data/Index_list_cleaned.csv"  # Ensure this CSV is in the same directory as the script
-    ETF_BASE_PATH = "data/ETFs"
-    BASE_PATH = "data"
-else:
-    FILE_PATH = "/home/dani/backtester/backtester-dani/data/Index_list_cleaned.csv"
-    ETF_BASE_PATH = "/home/dani/backtester/backtester-dani/data/ETFs"
-    BASE_PATH = "/home/dani/backtester/backtester-dani/data"
+
+FILE_PATH = "/home/dani/backtester/backtester-dani/data/Index_list_cleaned.csv"
+ETF_BASE_PATH = "/home/dani/backtester/backtester-dani/data/ETFs"
+BASE_PATH = "/home/dani/backtester/backtester-dani/data"
 
